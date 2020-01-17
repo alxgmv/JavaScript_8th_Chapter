b656306cef3dec3c4248699c3d7616d7f141660f
alxgmv <alxgmv@gmail.com>
2020-01-17 15:17:09 +0100

    update_1

---
+++ script.js
@@ -0,0 +1,15 @@
+// 'use strict'
+let javaCode = function() {
+  'use strict'
+  let x = prompt('Enter your code');
+  if (x!=null) {
+    try {
+      eval(x);
+      console.log(x);
+    } catch (err) {
+      alert('Error message: ' + err);
+      console.log(err);
+    }
+  }
+}
+javaCode();
