ea00c53df1862e585db3a1fd042ce43833ad6d94
alxgmv <alxgmv@gmail.com>
2020-01-24 16:48:22 +0100

    update_3

--- script_1.js
+++ script_1.js
@@ -1,19 +1,25 @@
 let filterByType = function() {
   let a = Array.prototype.slice.call(arguments);
-  console.log(a);
-  if ( a[0] == 'string' ) {
-    let stringArray = a.filter( function(a) { return typeof a == 'string' } );
-    console.log( stringArray );
+  let b = [];
+  for (i = 1; i < a.length; i++ ) {
+    if (typeof a[i] == a[0]) {
+      b.push(a[i]);
+    }
   }
-  if ( a[0] == 'number' ) {
-    let numericArray = a.filter( function(a) { return typeof a == 'number' } );
-    console.log( numericArray );
-  }
-  if ( a[0] == 'boolean') {
-    let booleanArray = a.filter( function(a) { return typeof a == 'boolean' } );
-    console.log( booleanArray );
-  }
+  console.log(b);
+  // if ( a[0] == 'string' ) {
+  //   let stringArray = a.filter( function(a) { return typeof a == 'string' } );
+  //   console.log( stringArray );
+  // }
+  // if ( a[0] == 'number' ) {
+  //   let numericArray = a.filter( function(a) { return typeof a == 'number' } );
+  //   console.log( numericArray );
+  // }
+  // if ( a[0] == 'boolean') {
+  //   let booleanArray = a.filter( function(a) { return typeof a == 'boolean' } );
+  //   console.log( booleanArray );
+  // }
 }
-filterByType('boolean', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
-filterByType('string', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
+// filterByType('boolean', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
+// filterByType('string', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
 filterByType('number', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
