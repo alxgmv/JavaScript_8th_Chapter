addd59bd6c5b5a920ee06f402b74afbf83d1b5ff
alxgmv <53427203+alxgmv@users.noreply.github.com>
2020-01-25 18:26:18 +0100

    update_5

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
+// let javaCode = function() {
+//   'use strict'
+//   let x = prompt('Enter your code');
+//   if (x!=null) {
+//     try {
+//       eval(x);
+//       console.log( x );
+//     } catch (err) {
+//       alert('Error message: ' + err);
+//       console.log(err);
+//     }
+//   }
+// }
+// javaCode();
+
+// let javaCode = function() {
+//   let x = prompt('Enter your code');
+//   let y = new Function(x);
+//   try {
+//     'use strict'
+//     y();
+//     console.log(y);
+//   } catch (err) {
+//     alert('Error message: ' + err);
+//     console.log(err);
+//   }
+// }
+// javaCode();
+
+
 let javaCode = function() {
-  'use strict'
-  let x = prompt('Enter your code');
-  if (x!=null) {
-    try {
-      eval(x);
-      console.log(x);
-    } catch (err) {
+  try {
+    let x = prompt('Enter your code');
+    let y = new Function(x);
+    return (y());
+  } catch (err) {
       alert('Error message: ' + err);
       console.log(err);
-    }
   }
 }
+'use strict'
 javaCode();
