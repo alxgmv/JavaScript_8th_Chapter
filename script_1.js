17a8caac3dac7986e5e880c908d576d29b1cbdf8
alxgmv <53427203+alxgmv@users.noreply.github.com>
2020-01-25 16:58:45 +0100

    update_4

--- script_1.js
+++ script_1.js
@@ -7,19 +7,7 @@ let filterByType = function() {
     }
   }
   console.log(b);
-  // if ( a[0] == 'string' ) {
-  //   let stringArray = a.filter( function(a) { return typeof a == 'string' } );
-  //   console.log( stringArray );
-  // }
-  // if ( a[0] == 'number' ) {
-  //   let numericArray = a.filter( function(a) { return typeof a == 'number' } );
-  //   console.log( numericArray );
-  // }
-  // if ( a[0] == 'boolean') {
-  //   let booleanArray = a.filter( function(a) { return typeof a == 'boolean' } );
-  //   console.log( booleanArray );
-  // }
 }
-// filterByType('boolean', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
-// filterByType('string', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
+filterByType('boolean', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
+filterByType('string', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
 filterByType('number', 'a', 'aa', 'vb', 2, 4,true, true, 'z', 343542);
